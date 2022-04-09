d46c0ed02a6fba67193c59705e044116a921c8a0
nelsonh <nelsonalbertohj@gmail.com>
2022-04-09 01:52:13 -0400

    Got split_folders code to make train_test splits

---
+++ split_folders.py
@@ -0,0 +1,4 @@
+import splitfolders
+splitfolders.ratio(r'C:\Users\nelso\My Drive (nelsonh@mit.edu)\Vision Dataset\imagenet-50',
+                   output=r'C:\Users\nelso\My Drive (nelsonh@mit.edu)\Vision Dataset\imagenet-50-splits',
+                   seed=1337, ratio=(.8, 0.0,0.2))
